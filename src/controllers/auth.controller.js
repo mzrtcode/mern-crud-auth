b7b2d520a4eb2341a732caa5220b333ad7414a30
Stiven Medina <stvnm33@gmail.com>
2023-07-02 21:38:19 -0500

    Creando login/logout

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -22,14 +22,35 @@ export const register = async (req, res) => {
 
     } catch (error) {
         res.status(500).json({message: error.message});
-    }
-
-
-     
+    }   
    
 }
 
-export const login = (req, res) => {
-    console.log(req.body)
-    res.send('Login...')
+export const login = async (req, res) => {
+    const {email, password} = req.body;
+   try {
+    const userFound = await User.findOne({email});
+    if(!userFound) return res.status(400).json({message: 'User not found'});
+
+       const isMatch = await bcrypt.compare(password,userFound.password);
+
+       if(!isMatch) return res.status(400).json({message: 'Incorrect password'});
+       const token = await createAccessToken({id: userFound._id});
+       res.cookie('token', token);
+       
+       res.send({
+           id: userFound._id,
+           username: userFound.username,
+           email: userFound.email
+       })
+
+   } catch (error) {
+       res.status(500).json({message: error.message});
+   }   
+  
+}
+
+export const logout  = async (req, res) => {
+    res.cookie('token', '', {expires: new Date(0)});
+    res.sendStatus(200);
 }
