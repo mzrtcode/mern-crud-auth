297abdc8684a289eee608910966a52ac103de137
Stiven M <stvnm33@gmail.com>
2023-07-03 20:23:02 -0500

    Task Crud

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import morgan from "morgan";
 import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.routes.js";
+import tasksRoutes from "./routes/tasks.routes.js";
 
 const app = express();
 
@@ -12,5 +13,6 @@ app.use(cookieParser());
 
 
 app.use('/api',authRoutes);
+app.use('/api',tasksRoutes);
 
 export default app;
---
+++ src/controllers/task.controller.js
@@ -0,0 +1,31 @@
+import Task from '../models/task.model.js';
+
+export const getTasks = async(req, res) => {
+    const tasks = await Task.find({user: req.user.id}).populate('user');
+    res.json(tasks);
+};
+export const getTask = async(req, res) => {
+    const task = await Task.findById(req.params.id).populate('user');
+    if(!task) return res.status(404).json({message: 'Task not found'});
+    return res.json(task);
+};
+export const createTask = async (req, res) => {
+    const {title, description, date} = req.body;
+    const task = new Task({
+        title,
+        description,
+        date,
+        user: req.user.id});
+    const createdTask = await task.save();
+    return res.json(createdTask);
+};
+export const updateTask = async(req, res) => {
+    const task = await Task.findByIdAndUpdate(req.params.id, req.body, {new: true});
+    if(!task) return res.status(404).json({message: 'Task not found'});
+    return res.json(task); 
+};
+export const deleteTask = async(req, res) => {
+    const task = await Task.findByIdAndDelete(req.params.id);
+    if(!task) return res.status(404).json({message: 'Task not found'});
+    return res.sendStatus(204);
+};
