68893c6f2e85ca46f95587ac1e6326c4a16c8e77
Stiven Medina <stvnm33@gmail.com>
2023-07-05 11:06:03 -0500

    Actualizar Tarea

--- client/src/context/TasksContext.jsx
+++ client/src/context/TasksContext.jsx
@@ -1,5 +1,5 @@
 import { createContext, useContext, useState } from "react";
-import { createTaskRequest, deleteTaskRequest, getTasksRequest } from "../api/task";
+import { createTaskRequest, deleteTaskRequest, getTaskRequest, getTasksRequest, updateTaskRequest } from "../api/task";
 
 const taskContext = createContext();
 
@@ -38,9 +38,27 @@ export function TaskProvider({ children }) {
     }
   }
 
+  const getTask=  async (id) => {
+    try {
+      const res = await getTaskRequest(id);
+    return (res.data)
+    } catch (error) {
+        console.log(error)
+    }
+  }
+
+  const updateTask = async (id, task) => {
+      try {
+        await updateTaskRequest(id, task);
+
+      } catch (error) {
+        console.log(error)
+      }
+  }
+
 
   return (
-    <taskContext.Provider value={{ tasks, createTask, getTasks, deleteTask }}>
+    <taskContext.Provider value={{ tasks, createTask, getTasks, deleteTask, getTask,updateTask }}>
       {children}
     </taskContext.Provider>
   );
--- client/src/pages/TaskFormPage.jsx
+++ client/src/pages/TaskFormPage.jsx
@@ -1,16 +1,37 @@
 import {useForm} from 'react-hook-form';
 import { useTasks } from '../context/TasksContext';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
+import { useEffect } from 'react';
 
 function TaskFormPage() {
 
-  const {register, handleSubmit} = useForm();
-  const {tasks, createTask} = useTasks();
+  const {register, handleSubmit, setValue} = useForm();
+  const {tasks, createTask, getTask, updateTask} = useTasks();
+  const {id} = useParams();
   const navigate = useNavigate();
 
+  useEffect(() =>{
+    async function loadTask(){
+      if(id){
+        const task = await getTask(id);
+        setValue('title', task.title);
+      setValue('description', task.description);
+      }
+      
+    }
+
+    loadTask();
+  },[])
+
   const onSubmit = handleSubmit((data) =>{
-    createTask(data);
+    if(!id){
+      createTask(data);
+    }else{
+      updateTask(id, data);
+    }
+    
     navigate('/tasks');
+   
   })
    return (
     <div className='bg-zinc-800 max-w-md w-full p-10 rounded-md'>
